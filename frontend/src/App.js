b101a065b0eb9eec2f3709fd6f6557636ec899fe
Supletsov Dmitriy <supletsovd@gmail.com>
2024-07-22 10:37:22 +0700

    refresh implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
-
 import AuthenticationProvider from "./components/auth/AuthenticationProvider";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Main from "./components/app/Main";
+import {refreshToken} from "./tools/utils";
 
 
 function App() {
-  const [authenticated, setAuthenticated] = useState(false);
+    const [authenticated, setAuthenticated] = useState(false);
 
-  //todo: add check if user has access or refresh token in local storage and if one of them still valid
+    useEffect(  () => {
+        const checkAuthentication = async () => {
+            const isAuthenticated = await refreshToken();
+            setAuthenticated(isAuthenticated);
+        };
+        checkAuthentication().then(r => console.log("token refreshed"));
+    }, [])
 
-  return (
-      authenticated ? <Main setAuthenticated={setAuthenticated}/> : <AuthenticationProvider setAuthenticated={setAuthenticated}/>
-  );
+
+    return (
+        authenticated ? <Main setAuthenticated={setAuthenticated}/> :
+            <AuthenticationProvider setAuthenticated={setAuthenticated}/>
+    );
 }
 
 export default App;
--- frontend/src/tools/utils.js
+++ frontend/src/tools/utils.js
@@ -1,9 +1,6 @@
-/**
- * Parses an otpauth URL and returns an object with the parsed information.
- *
- * @param {string} otpauthUrl - The otpauth URL to parse.
- * @returns {object} - An object containing the appName, username, secret, and issuer.
- */
+import {jwtDecode} from "jwt-decode";
+import {REFRESH_URL} from "./urls";
+
 export function parseOtpAuthUrl(otpauthUrl) {
     const regex = /^otpauth:\/\/totp\/([^:]+):([^?]+)\?secret=([^&]+)&issuer=([^&]+)$/;
     const match = otpauthUrl.match(regex);
@@ -17,5 +14,57 @@ export function parseOtpAuthUrl(otpauthUrl) {
         };
     } else {
         throw new Error("Invalid otpauth URL format");
+    }
+}
+
+export function isTokenExpired(token) {
+    try {
+        const decodedToken = jwtDecode(token);
+        const currentTime = Math.floor(Date.now() / 1000);
+        return decodedToken.exp < currentTime;
+    } catch (error) {
+        console.error('Invalid token:', error);
+        return true;
+    }
+}
+
+export async function refreshToken() {
+    if (localStorage.getItem("tokens") !== null) {
+        let tokens = JSON.parse(localStorage.getItem("tokens"));
+        const refreshToken = tokens.refreshToken;
+        const accessToken = tokens.accessToken;
+
+        if (!isTokenExpired(accessToken)) {
+            return true;
+        } else if (!isTokenExpired(refreshToken)) {
+            try {
+                const response = await fetch(REFRESH_URL, {
+                    method: "POST",
+                    body: JSON.stringify({refreshToken: refreshToken}),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
+
+                if (!response.ok) {
+                    const errorText = await response.text();
+                    throw new Error(errorText);
+                }
+
+                const data = await response.json();
+                localStorage.setItem("tokens", JSON.stringify({
+                    refreshToken: refreshToken,
+                    accessToken: data.accessToken
+                }));
+                return true;
+            } catch (error) {
+                console.error('Error refreshing token:', error);
+                return false;
+            }
+        } else {
+            return false;
+        }
+    } else {
+        return false;
     }
 }
